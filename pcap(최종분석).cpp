8523d0966881528cb71a6d77891677080251d24b
mailinnence <78959189+mailinnence@users.noreply.github.com>
2023-07-21 20:48:54 +0900

    Update and rename pcap(최종분).cpp to pcap(최종분석).cpp

--- pcap(최종분석).cpp
+++ pcap(최종분석).cpp
@@ -150,13 +150,16 @@ int main(int argc, char* argv[]) {
         printf("\n\n");
 
 
+
+
+
+	    
         // IP 헤더를 추출하여 출력
         // 코드에서 packet은 패킷 데이터가 저장된 포인터이고, sizeof(struct EthernetHeader)는 Ethernet 헤더의 크기를 나타냅니다. 
         // 이 코드는 Ethernet 헤더를 건너뛰고 IP 헤더의 시작 위치를 찾기 위해 packet 포인터에 Ethernet 헤더의 크기를 더하는 것
-
-
         // ip 구조체는 #include <netinet/ip.h> // for ip header 안에 있는 구조체를 가지고 와서 필요한 정보만 담는다.
         // 구조는 이러하다
+	    
         /*
             ip_hl: 헤더 길이 (Header Length)를 나타냅니다. 4바이트 단위로 표현되며, 최대 15를 나타냅니다.
             ip_v: IP 버전을 나타냅니다. IPv4의 경우 4로 설정됩니다.
@@ -170,8 +173,6 @@ int main(int argc, char* argv[]) {
             ip_src: 송신자(IP 출발지 주소)를 나타냅니다.
             ip_dst: 수신자(IP 도착지 주소)를 나타냅니다.
         */
-
-
       
         // ip 헤더를 추출하여 출력
       	struct ip* ip_header = (struct ip*)(packet + sizeof(struct EthernetHeader));
@@ -183,23 +184,90 @@ int main(int argc, char* argv[]) {
       	printf("\n\n");
 
  
-        // ip 헤더 출력
-    	// struct ip* ip_header = (struct ip*)(packet + sizeof(struct EthernetHeader));
-    	// printf("IP Header\n");
-    	// printf("Src IP: ");
-    	// print_ip_address(&ip_header->ip_src);
-    	// printf("  Dst IP: ");
-    	// print_ip_address(&ip_header->ip_dst);
+        // ip 헤더 헥스값값 출력
+        // printf("IP Header\n");
+        // printf("Src IP: ");
+        // print_ip_hex(&ip_header->ip_src);
+        // printf("  Dst IP: ");
+        // print_ip_hex(&ip_header->ip_dst);
+        // printf("\n\n");
     	// printf("\n\n");
 
-	      struct tcphdr* tcp_header = (struct tcphdr*)(packet + sizeof(struct EthernetHeader) + (ip_header->ip_hl << 2));
-        printf("TCP Header\n");
+
+
+
+
+        // tcphdr 구조체는 #include <netinet/tcp.h> // for tcp header 안에 있는 구조체를 가지고 와서 필요한 정보만 담는다.
+        // 구조는 이러하다
+        /*
+        th_sport: 송신자 포트 번호를 나타냅니다.
+        th_dport: 수신자 포트 번호를 나타냅니다.
+        th_seq: 시퀀스 번호를 나타냅니다.
+        th_ack: 확인 응답 번호를 나타냅니다.
+        th_off: 데이터 오프셋을 나타냅니다. 4바이트 단위로 표현되며, 최대 15를 나타냅니다.
+        th_flags: 플래그 필드를 나타냅니다. FIN, SYN, RST, PUSH, ACK, URG 플래그 등이 포함됩니다.
+        th_win: 윈도우 크기를 나타냅니다.
+        th_sum: TCP 헤더의 체크섬 값을 나타냅니다.
+        th_urp: 긴급 포인터를 나타냅니다.
+        */
+	    
+	struct tcphdr* tcp_header = (struct tcphdr*)(packet + sizeof(struct EthernetHeader) + (ip_header->ip_hl << 2));
+	/*
+
+	궁금점..!
+	struct tcphdr* tcp_header = (struct tcphdr*)(packet + sizeof(struct EthernetHeader) + (ip_header->ip_hl << 2));
+	
+	이것과
+	
+	struct tcphdr* tcp_header = (struct tcphdr*)(packet + sizeof(struct EthernetHeader) + sizeof(struct ip_header) );
+	
+	이거는 같아야 하지 않을까????
+	
+	다르다 왜냐면 EthernetHeader 는 그 크기가 고정되어 있지만 ip 경우는 그 크기가 각기 다를 수 있다
+	때문에  ip_hl: 헤더 길이 (Header Length) 를 이용해서 크기를 가져와야 정확한 위치를 구할 수 있다.
+	sizeof() 와 더하야 하기 때문에 <<2 작업이 필요하다
+
+	gpt 가라사대 --------------------------------------------------------------------------------------------------------------
+	<< 2 연산을 사용하여 IP 헤더의 워드 단위를 바이트 단위로 변환하는 이유는 TCP 헤더와 Payload의 위치를 계산하기 위함입니다. 
+ 	TCP 헤더는 IP 헤더 이후에 위치하며, IP 헤더의 길이를 바이트 단위로 계산하기 위해 << 2 연산을 사용하여 올바른 위치를 찾아냅니다. 
+  	따라서 packet + sizeof(struct EthernetHeader) + (ip_header->ip_hl << 2)는 TCP 헤더의 시작 위치를 가리키게 됩니다.
+	---------------------------------------------------------------------------------------------------------------------------
+	*/
+	
+	printf("TCP Header\n");
         printf("Src Port: %u  Dst Port: %u", ntohs(tcp_header->th_sport), ntohs(tcp_header->th_dport));
         printf("\n\n");
 
 
+
+	    
+
         // Payload(Data)의 hexadecimal value 출력
+	// 네트워크 패킷에서 Payload 또는 Data 부분의 16진수 값을 출력하는 부분을 담당합니다. 
+	// Payload는 네트워크 패킷에서 유저 데이터가 담겨 있는 부분을 의미합니다. 
+	// 네트워크 패킷은 헤더(Header)와 Payload(데이터)로 구성되어 있으며, 
+	// 헤더는 네트워크 장비가 패킷을 전달하고 처리하는 데 필요한 정보를 담고 있습니다. 
+	// Payload는 실제 데이터를 포함하고 있으며, 예를 들어 웹 페이지의 HTML 내용, 파일의 내용 등이 Payload로 전송됩니다.
+
+	    
         const u_char* payload = packet + sizeof(struct EthernetHeader) + (ip_header->ip_hl << 2) + (tcp_header->th_off << 2);
+
+	// 네트워크 패킷에서 Payload 또는 Data 부분의 16진수 값을 출력하는 부분을 담당합니다. 
+	// Payload는 네트워크 패킷에서 유저 데이터가 담겨 있는 부분을 의미합니다. 
+	// 네트워크 패킷은 헤더(Header)와 Payload(데이터)로 구성되어 있으며, 
+	// 헤더는 네트워크 장비가 패킷을 전달하고 처리하는 데 필요한 정보를 담고 있습니다. 
+	// Payload는 실제 데이터를 포함하고 있으며, 예를 들어 웹 페이지의 HTML 내용, 파일의 내용 등이 Payload로 전송됩니다.
+
+	// ip 에 포함되어 있는 Payload 정보를 추출하려면 tpc 위치 시작 위치에서 빼가는 방식으로 접근한다.
+	// 실제 크기로 뺼수 있도록 네트워크 바이트 순서에서 호스트 바이트 순서로 16비트(short) 정수 값을 변환하는 함수 ntohs을 이용한다
+	/*
+	    ip_hl: 헤더 길이 (Header Length)를 나타냅니다. 4바이트 단위로 표현되며, 최대 15를 나타냅니다.
+            ip_off: 패킷의 조각화 정보를 나타냅니다.
+        
+	    을 빼면 payload 값이 나오게 되고 그 중 10바이트 까지만 출력한다.
+	*/
+	    
+	    
         int payload_len = ntohs(ip_header->ip_len) - (ip_header->ip_hl << 2) - (tcp_header->th_off << 2);
         printf("Payload(Data) (Hexadecimal Value):\n");
         for (int i = 0; i < payload_len && i < 10; i++) {
